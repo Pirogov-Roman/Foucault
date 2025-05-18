22d37246a7d903c2f85f95728ad6f3dab0ac0347
Pirogov-Roman <romapirog2005@gmail.com>
2025-05-18 23:26:26 +0300

    ggg

--- Foucaultzip/Foucault/foucault_pendulum/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # Добавьте include в импорт
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pendulum.urls')),  # Подключение URL-ов приложения
-]
